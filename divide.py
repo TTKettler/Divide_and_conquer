d41624fa5560fcf7f44c2eabf06fbea05dcfbe39
TTKettler <79258681+TTKettler@users.noreply.github.com>
2024-02-27 12:50:27 +0100

    added documentation to divide.py

--- divide.py
+++ divide.py
@@ -1,6 +1,7 @@
+#Double division function for dividend and divisor
 def double_integer_division(dividend, divisor):
     return dividend // divisor //divisor
 
-
+#Multiplication function for a and b
 def multiply_fun(a, b):
     return (a*b)
